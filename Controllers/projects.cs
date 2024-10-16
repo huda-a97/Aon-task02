8d5ee97197cb306b8a67e77e0a5416ffafe0eae2
huda-a97 <huda.a.fawzi@gmail.com>
2024-10-16 22:16:16 +0300

    task done

---
+++ Controllers/projects.cs
@@ -0,0 +1,61 @@
+﻿using Aon_Freelance.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aon_Freelance.Controllers
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class projects : ControllerBase
+    {
+        private static List<Project> AllProjects = new List<Project>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(AllProjects);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Project pro)
+        {
+            AllProjects.Add(pro);
+
+            return CreatedAtAction("Create", new { Id = pro.Id }, AllProjects);
+        }
+
+       
+
+        [HttpGet ("{id}")]
+
+        public IActionResult GetSpecific(int id)
+        {
+            Project project = AllProjects.FirstOrDefault(project => project.Id == id);
+
+            if(project == null)
+            {
+                return NotFound("the project not found");
+            }
+
+            return Ok(project);
+        }
+
+
+        [HttpDelete ("{id}")]
+
+        public IActionResult DeleteProject(int id)
+        {
+            Project project = AllProjects.FirstOrDefault(project => project.Id == id);
+
+            if (project == null)
+            {
+                return NotFound("the project not found");
+            }
+
+            AllProjects.Remove(project);
+            return Ok("the project was deleted");
+           
+        }
+    }
+
+}
---
+++ models/Project.cs
@@ -0,0 +1,8 @@
+﻿namespace Aon_Freelance.models
+{
+    public class Project
+    {
+       public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
